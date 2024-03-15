6d947ebdb875191d2eec7af7b2b98011ab35306c
Mariana Martins <marih_ms@hotmail.com>
2024-03-14 21:34:53 -0300

    update aula 3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  listaProdutos = [
+  {
+    imagem: 'assets/pera.jpg',
+    produto: 'pera',
+    valor: '3,50'
+  },
+  {
+    imagem: 'assets/uva.jpg',
+    produto: 'uva',
+    valor: '7,50'
+  },
+  {
+    imagem: 'assets/maca.jpg',
+    produto: 'maçã',
+    valor: '5,00'
+  }
+]
 }
