c008bd2b8542cd9e5590f3e85c1fea5cc4b3671a
Mariana Martins <marih_ms@hotmail.com>
2024-03-17 18:02:29 -0300

    update aula 14-03

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CardComponent } from './card/card.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'home',
         component: HomeComponent  
     },
     {
         path: 'card',
         component: CardComponent
+    },
+
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent, CommonModule],
+  imports: [CardComponent, CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  textoPesquisa: string | undefined;
+  pesquisar() {
+    this.listaProdutosFiltro = this.listaProdutos.filter(item => item.fruta === this.textoPesquisa)
+  }
   listaProdutos = [
   {
     imagem: 'assets/pera.jpg',
@@ -25,6 +31,12 @@ export class HomeComponent {
     imagem: 'assets/maca.jpg',
     fruta: 'maçã',
     valor: '5,00'
+  },
+  {
+    imagem: 'assets/banana.jpg',
+    fruta: 'banana',
+    valor: '2,50'
   }
 ]
+  listaProdutosFiltro = this.listaProdutos;
 }
